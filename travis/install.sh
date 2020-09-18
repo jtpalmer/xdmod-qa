d0ae158730d4df7c674831c7374c85412371b312
Jeffrey T. Palmer <jtpalmer@buffalo.edu>
2020-09-18 09:17:08 -0400

    Just a test

--- travis/install.sh
+++ travis/install.sh
@@ -80,7 +80,8 @@ if [ "$repo_type" == "module" ]; then
     start_travis_fold open-xdmod
     echo "Obtaining and integrating with Open XDMoD code..."
 
-    xdmod_branch="${XDMOD_MAIN_BRANCH:-$TRAVIS_BRANCH}"
+    #xdmod_branch="${XDMOD_MAIN_BRANCH:-$TRAVIS_BRANCH}"
+    xdmod_branch="xdmod9.5"
     echo "Cloning Open XDMoD branch '$xdmod_branch'"
     git clone --depth=1 --branch="$xdmod_branch" https://github.com/ubccr/xdmod.git "$XDMOD_SOURCE_DIR"
 
